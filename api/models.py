6d97f1bb12d8f3d683dc75b37f3591dd3bb81c50
JaroDomcio <281053@student.pwr.edu.pl>
2025-09-29 14:14:16 +0200

    feat: add models

---
+++ api/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+
+    class Status(models.TextChoices):
+        TO_DO = 'TO_DO', 'to_do',
+        DONE = 'DONE', 'done',
+        IN_PROGRESS = 'IN_PROGRESS', 'in_progress',
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.TO_DO)
+    deadline = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
