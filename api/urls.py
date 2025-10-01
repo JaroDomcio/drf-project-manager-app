f4437c467ceb20c406f1c1a2702cfd0df0fa7b16
JaroDomcio <281053@student.pwr.edu.pl>
2025-10-01 21:47:00 +0200

    feat: add jwt authentication configuration

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ProjectViewSet, TaskViewSet, CommentViewSet
+from rest_framework_simplejwt import views as jwt_views
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -10,4 +11,7 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
