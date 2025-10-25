a81722245a2a03238b56f7e403ea1b3d1a226ab1
JaroDomcio <281053@student.pwr.edu.pl>
2025-10-25 20:25:16 +0200

    feat: add IsAuthorOrReadOnly permission

--- api/permissions.py
+++ api/permissions.py
@@ -16,3 +16,16 @@ class IsManagerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.role == 'MANAGER')
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of an object to edit it.
+    Assumes the model instance has an 'author' or 'owner' attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
