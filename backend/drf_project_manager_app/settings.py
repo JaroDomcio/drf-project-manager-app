597a32d842b223e1db49bfc0a82927577d1a4acd
JaroDomcio <281053@student.pwr.edu.pl>
2025-11-03 20:23:58 +0100

    chore: reset Tracked files to apply .gitignore

--- backend/drf_project_manager_app/settings.py
+++ backend/drf_project_manager_app/settings.py
@@ -42,13 +42,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'django_filters',
-    'drf_spectacular'
+    'drf_spectacular',
+    'corsheaders',
 
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
