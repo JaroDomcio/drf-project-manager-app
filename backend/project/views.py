cd249be0de043af7b5daa9539c74efdb74e64020
unknown <281053@student.pwr.edu.pl>
2025-11-15 23:34:25 +0100

    fix(backend): add missing Q import

--- backend/project/views.py
+++ backend/project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from django.db.models import Q
 from rest_framework import viewsets, permissions, status, filters
 from .models import Project
 from .serializers import ProjectSerializer
