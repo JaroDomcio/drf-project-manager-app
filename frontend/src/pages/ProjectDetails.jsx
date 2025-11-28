7e118cac00344e5d88a58c9bacecfb73bb14fb28
unknown <281053@student.pwr.edu.pl>
2025-11-28 22:55:21 +0100

    feat(frontend): add members for project details

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -6,12 +6,16 @@ import '../css/ProjectDetails.css'
 function ProjectDetails() {
     const {projectId} = useParams();
     const [project, setProject] = useState(null);
+    const [members,setMembers] = useState(null);
 
     useEffect(() => {
         const fetchProjectDetails = async () => {
             try {
-                const response = await apiClient.get(`/projects/${projectId}/`);
-                setProject(response.data);
+                const ProjectResponse = await apiClient.get(`/projects/${projectId}/`);
+                const MembersResponse = await apiClient.get(`/projects/${projectId}/members`)
+
+                setProject(ProjectResponse.data);
+                setMembers(MembersResponse.data)
             } catch (error) {
                 console.error("Error fetching project details:", error);
             }
@@ -23,15 +27,30 @@ function ProjectDetails() {
         return <div>Ten projekt nie istnieje</div>;
     }
 
+
     return (
     <div className='project-details-container'> 
         <div className='project-details-box'>
             <h1 className='project-detail-title'>{project.title}</h1>
             <div className='project-details-content'>
-                <div className='project-details-members'>
-                    <h3>Członkowie</h3>
+                <div>
+                    <div className='project-details-members'>
+                        <h3>Członkowie</h3>
+                        {members.length === 0 ? ("Brak członków"): 
+                            (
+                                members.map((member) => (
+                                    <div key={member.id} className="member-list">
+                                        <p className='member-list-name'>{member.first_name} {member.last_name}</p>
+                                    </div>
+                                )
+                                )
+                            )
+                        }
+                    </div>
+
                 </div>
                 <div className='project-details-description'> 
+                    <h3>Opis projektu</h3>
                     {project.description}
                 </div>
             
