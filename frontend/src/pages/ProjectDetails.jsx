a994e5e0af7e72254b02b2e96dc8686d1065c1ed
unknown <281053@student.pwr.edu.pl>
2025-11-29 18:27:40 +0100

    feat(frontend): add modal component

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -2,11 +2,15 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import apiClient from "../api/apiClient";
 import '../css/ProjectDetails.css'
+import Modal from '../components/Modal.jsx'
 
 function ProjectDetails() {
     const {projectId} = useParams();
     const [project, setProject] = useState(null);
     const [members,setMembers] = useState(null);
+
+    const [isMyTasksOpen, setMyTasksOpen] = useState(false);
+    const [isStatisticsOpen, setStatisticsOpen] = useState(false)
 
     useEffect(() => {
         const fetchProjectDetails = async () => {
@@ -27,20 +31,22 @@ function ProjectDetails() {
         return <div>Ten projekt nie istnieje</div>;
     }
 
-    const handleTasksClick = () => {
-        alert('Task click')
-    }
+    // const handleTasks = () => {
+    //     alert('Task click')
+    // }
 
-    const handleStatsClick = () => {
-        alert('stats click')
-    }
+    // const handleStats = () => {
+    //     alert('stats click')
+    // }
 
 
     return (
     <div className='project-details-container'>
         <div className="project-details-options-container">
-            <p className="project-details-option" onClick = { () => handleTasksClick()}  >Moje zadania</p>
-            <p className="project-details-option" onClick = { () => handleStatsClick()} >Statystyki</p>
+            <p className="project-details-option" onClick = { () => setMyTasksOpen(true)} >Moje zadania</p>
+            <p className="project-details-option" onClick = { () => setStatisticsOpen(true)} >Statystyki</p>
+            {/* <p className="project-details-option" onClick = { () => handleAddTask()} >Dodaj zadanie</p>
+            <p className="project-details-option" onClick = { () => handleAddMember()} >Dodaj członka</p> */}
         </div>
         <div className='project-details-box'>
             <h1 className='project-detail-title'>{project.title}</h1>
@@ -72,6 +78,13 @@ function ProjectDetails() {
 
             </div>
         </div>
+    <Modal isOpen ={isMyTasksOpen} onClose={() => setMyTasksOpen(false)}>
+        
+    </Modal>
+
+    <Modal isOpen ={isStatisticsOpen} onClose={() => setStatisticsOpen(false)}>
+        
+    </Modal>
     </div>
 )
 };
